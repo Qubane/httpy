d0867343856568fec2ca3c4dca6555bae1c52548
UltraQbik <no1skill@yandex.ru>
2024-08-25 17:49:01 +0300

    I hate JS

--- www/scripts/test.js
+++ www/scripts/test.js
@@ -1,9 +1,14 @@
 const canvas = document.getElementById("JSCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.strokeStyle = "blue"
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(511, 511);
-ctx.stroke();
+function draw() {
+    ctx.strokeStyle = "#" + Math.floor(Math.random() * 16777216).toString(16).padStart(6, '0')
+    ctx.beginPath();
+    ctx.moveTo(Math.random() * 511, Math.random() * 511);
+    ctx.lineTo(Math.random() * 511, Math.random() * 511);
+    ctx.stroke();
+}
 
+for (let i = 0; i < 50; i++) {
+    draw()
+}
