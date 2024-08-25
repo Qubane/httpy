1793440e0db1688ed5c4d71208d5f8e2ab0b2a0f
UltraQbik <no1skill@yandex.ru>
2024-08-25 15:34:28 +0300

    JS Stuff

--- www/scripts/test.js
+++ www/scripts/test.js
@@ -1,1 +1,9 @@
-console.log('прощай world')
+const canvas = document.getElementById("JSCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "blue"
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(511, 511);
+ctx.stroke();
+
