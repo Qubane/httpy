d0d032c9570507da419925db33b6023f046d011e
UltraQbik <no1skill@yandex.ru>
2024-09-13 16:45:11 +0300

    Add structures.py

---
+++ src/structures.py
@@ -0,0 +1,16 @@
+class Request:
+    """
+    HTTP request
+    """
+
+    def __init__(self, raw_http: bytes):
+        pass
+
+
+class Response:
+    """
+    HTTP response
+    """
+
+    def __init__(self):
+        pass
