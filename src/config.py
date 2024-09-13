908735a5eb5d4665ece8c761c4299a9feec42e31
UltraQbik <no1skill@yandex.ru>
2024-09-13 20:19:48 +0300

    Update config files

--- src/config.py
+++ src/config.py
@@ -10,6 +10,15 @@ class Config:
     LOGGING_PATH: str
     LOGGING_MAX_SIZE: int
 
+    HTTP_MAX_PATH_LENGTH: int
+    HTTP_MAX_ARG_NUMBER: int
+    HTTP_MAX_HEADER_NUMBER: int
+
+    THREADING_MAX_NUMBER: int
+    THREADING_TIMEOUT: int
+
+    API_VERSIONS: list[str]
+
     @classmethod
     def _read_configs(cls):
         """
@@ -18,4 +27,7 @@ class Config:
 
         import os
         import json
+        if not os.path.exists("cfg/config.json"):
+            raise FileNotFoundError("Missing config")
+
 
