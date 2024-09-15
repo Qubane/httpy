5813d39c7b934654bbc5a0451101801b01d47a08
UltraQbik <no1skill@yandex.ru>
2024-09-16 02:17:13 +0300

    Working file manager & fixes to FileContainer class Fixed file&directory creation for FileContainer Added file compression to FileContainer Finished path updating for FileManager

--- src/fileman.py
+++ src/fileman.py
@@ -5,7 +5,23 @@ File Manager for controlling access to files
 
 import os
 from collections.abc import Generator
+
 from src.config import Config
+
+
+def list_directory(dirpath: str) -> list[str]:
+    """
+    Lists all file in given directory. Returns single file if dirpath is path to a file
+    :param dirpath: path to directory
+    :return: list of paths to files
+    """
+
+    if os.path.isfile(dirpath):  # if dirpath is a file -> return
+        return [dirpath]
+    paths = []
+    for file in os.listdir(dirpath):
+        paths += list_directory(os.path.join(dirpath, file))
+    return paths
 
 
 class File:
@@ -75,6 +91,14 @@ class File:
                 while data := file.read(Config.SOCKET_SEND_SIZE):
                     yield data
 
+    @property
+    def filepath(self) -> str:
+        return self._filepath
+
+    @property
+    def filetype(self) -> str:
+        return self._filetype
+
 
 class FileContainer:
     """
@@ -86,10 +110,47 @@ class FileContainer:
         self.compressed: bool = compress
 
         if compress:  # if compression is enabled
+            # gzip compression
             c_filepath = os.path.join(Config.FILEMAN_COMPRESS_PATH, "gzip", filepath)
+            if not os.path.exists(c_filepath):  # ensure existence
+                if not os.path.exists(os.path.dirname(c_filepath)):
+                    os.makedirs(os.path.dirname(c_filepath))
+                with open(c_filepath, "wb") as f:
+                    f.write(b'file')
             self.gzip_compressed: File = File(c_filepath, cached=cache)
+
+            # brotli compression
             c_filepath = os.path.join(Config.FILEMAN_COMPRESS_PATH, "brotli", filepath)
+            if not os.path.exists(c_filepath):
+                if not os.path.exists(os.path.dirname(c_filepath)):
+                    os.makedirs(os.path.dirname(c_filepath))
+                with open(c_filepath, "wb") as f:
+                    f.write(b'file')
             self.brotli_compressed: File = File(c_filepath, cached=cache)
+
+            # actually compress files
+            self.compress_files()
+
+    def compress_files(self) -> None:
+        """
+        (Re)Compresses files
+        """
+
+        if not self.compressed:
+            return
+
+        import gzip
+        import brotli
+        chunk_size = 2**20 * 64  # 64 MiB
+        with gzip.open(self.gzip_compressed.filepath, "wb") as compressed:
+            with open(self.uncompressed.filepath, "rb") as file:
+                compressed.writelines(file)
+        with open(self.brotli_compressed.filepath, "wb") as compressed:
+            br = brotli.Compressor()
+            with open(self.uncompressed.filepath, "rb") as file:
+                while data := file.read(chunk_size):
+                    br.process(data)
+                    compressed.write(br.flush())
 
 
 class FileManager:
@@ -97,5 +158,45 @@ class FileManager:
     File Manager (FileMan)
     """
 
-    def __init__(self):
-        self._path_map: dict
+    def __init__(
+            self,
+            allow_compression: bool = False,
+            cache_everything: bool = False):
+        """
+        File manager class
+        :param allow_compression: allows file compression
+        :param cache_everything: caches EVERY file (do not use for big sites)
+        """
+
+        self._path_map: dict[str, FileContainer] = dict()
+        # {webpath: FileContainer, webpath: FileContainer, ...}
+
+        self._allow_compression: bool = allow_compression
+        self._cache_everything: bool = cache_everything
+
+    def update_paths(self):
+        """
+        Update's path map
+        """
+
+        import json
+        if not os.path.isfile("cfg/paths.json"):
+            raise FileNotFoundError("Missing path config")
+        with open("cfg/paths.json", "r", encoding="utf-8") as file:
+            paths: dict[str, dict[str, str | bool]] = json.loads(file.read())
+
+        for key, val in paths.items():
+            if key[-1] == "*":  # '*' path
+                web_dirpath = key[:-1]
+                real_dirpath = val["path"][:-1]
+                for filepath in list_directory(real_dirpath):
+                    web_filepath = f"{web_dirpath}{filepath[len(real_dirpath):]}"
+                    self._path_map[web_filepath] = FileContainer(
+                        filepath=filepath,
+                        compress=val.get("compress", self._allow_compression),
+                        cache=val.get("cache", self._cache_everything))
+            else:  # direct path
+                self._path_map[key] = FileContainer(
+                    filepath=val["path"],
+                    compress=val.get("compress", self._allow_compression),
+                    cache=val.get("cache", self._cache_everything))
