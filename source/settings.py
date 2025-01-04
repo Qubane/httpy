1aa3b5ecb12773146b7f46eba750becb5a015d9d
UltraQbik <no1skill@yandex.ru>
2025-01-04 15:04:35 +0300

    Add logger

---
+++ source/settings.py
@@ -0,0 +1,31 @@
+import os
+import logging
+import logging.handlers
+
+
+VARS_DIRECTORY = "var"
+LOGS_DIRECTORY = "logs"
+
+
+def init():
+    # create 'var' directory
+    if not os.path.isdir(VARS_DIRECTORY):
+        os.mkdir(VARS_DIRECTORY)
+
+    # create 'logs' directory
+    if not os.path.isdir(LOGS_DIRECTORY):
+        os.mkdir(LOGS_DIRECTORY)
+
+    # setup logging
+    logging.basicConfig(
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{",
+        format="[{asctime}] [{levelname:<8}] {name}: {message}",
+        handlers=[
+            logging.handlers.RotatingFileHandler(
+                filename=f"{LOGS_DIRECTORY}/discord.log",
+                encoding="utf-8",
+                maxBytes=2**20 * 32,  # 32 MiB
+                backupCount=5),
+            logging.StreamHandler()])
