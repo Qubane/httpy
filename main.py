f106187c1beb17d0952e9e45227fe294d401827c
UltraQbik <no1skill@yandex.ru>
2025-01-14 18:48:04 +0300

    change from 80 to 8080 port

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def parse_args():
 def main():
     # args = parse_args()
     httpy = HTTPyServer(
-        bind_address=("0.0.0.0", 80))
+        bind_address=("0.0.0.0", 8080))
     httpy.run()
 
 
