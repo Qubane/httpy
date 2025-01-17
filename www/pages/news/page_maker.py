91fa90d8ee28f16f99ac5b201ff6726b0054a92a
UltraQbik <no1skill@yandex.ru>
2025-01-17 19:51:22 +0300

    md2html parser

--- www/pages/news/page_maker.py
+++ www/pages/news/page_maker.py
@@ -31,4 +31,59 @@ def make_page(**kwargs) -> Generator[bytes, Any, None]:
                 configs[config[0].strip("-")] = config[1].replace("\r", "").replace("\n", "")
             page_content = file.read()
 
-    yield b'Sorry, not implemented yet :<'
+    yield b'yes'
+
+
+def parse_md2html(text: str) -> str:
+    """
+    Parses md to html
+    :param text: md formatted string
+    :return: html formatted string
+    """
+
+    text = text.replace("\r", "").split("\n")
+
+    # make formatting
+    is_bold = False
+    is_italics = False
+    for _ in range(len(text)):
+        line = text.pop(0)
+        new_line = ""
+        format_buffer = ""
+        for char in line:
+            if char == "*":
+                format_buffer += char
+                continue
+            if format_buffer == "*" and char != "*":
+                format_buffer = ""
+                is_italics = not is_italics
+                if is_italics:
+                    new_line += "<i>"
+                else:
+                    new_line += "</i>"
+            if format_buffer == "**" and char != "*":
+                format_buffer = ""
+                is_bold = not is_bold
+                if is_italics:
+                    new_line += "<b>"
+                else:
+                    new_line += "</b>"
+            new_line += char
+        text.append(new_line)
+
+    # make headers
+    for _ in range(len(text)):
+        line = text.pop(0)
+        if len(line) == 0:
+            continue
+        if line[0] == "#":
+            header = 1
+            if len(line) > 2 and line[1] == "#":
+                header = 2
+            if len(line) > 3 and line[2] == "#":
+                header = 3
+            text.append(f"<p><h{header}>{line[header:].strip()}</h{header}></p>")
+            continue
+        text.append(f"<p>{line.strip()}</p>")
+
+    return "".join(text)
