8c101f2e8fe8a9c97acf7661e5e8fec8880dedcb
UltraQbik <no1skill@yandex.ru>
2025-01-18 19:50:29 +0300

    Change to use ClientSideErrors subclass

--- source/exceptions.py
+++ source/exceptions.py
@@ -1,3 +1,12 @@
-class NotFound(Exception):
+class ClientSideErrors(Exception):
+    """
+    It's all users fault :)
+    """
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class NotFound(ClientSideErrors):
     def __init__(self, *args):
         super().__init__(*args)
