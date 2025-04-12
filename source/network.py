4de4ec5ed3dbbf893bce18ac86819b959142cc7b
UltraQbik <no1skill@yandex.ru>
2025-04-12 18:02:13 +0300

    add fetch_request function

--- source/network.py
+++ source/network.py
@@ -1,3 +1,15 @@
 """
 Networking stuff
 """
+
+
+import asyncio
+from source.classes import Connection
+
+
+async def fetch_request(con: Connection) -> Request:
+    """
+    Fetches request from a connection
+    :param con: client connection
+    :return: client request
+    """
