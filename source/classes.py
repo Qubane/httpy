5adb330b7653338e2d7a51ffe0e6212453c2cd44
UltraQbik <no1skill@yandex.ru>
2025-04-12 18:01:07 +0300

    add Connection class

--- source/classes.py
+++ source/classes.py
@@ -3,6 +3,15 @@ Classes definitions
 """
 
 
+class Connection:
+    """
+    Connection wrapper
+    """
+
+    def __init__(self):
+        ...
+
+
 class Request:
     """
     Client's HTTP request
