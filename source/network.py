f84272d2d67a9279eba6e6e270f3f5cca1a11b45
UltraQbik <no1skill@yandex.ru>
2025-04-12 19:27:32 +0300

    steal code from main branch

--- source/network.py
+++ source/network.py
@@ -4,6 +4,7 @@ Networking stuff
 
 
 from source.classes import *
+from source.options import *
 
 
 async def fetch_request(con: Connection) -> Request:
@@ -21,3 +22,25 @@ async def send_response(con: Connection, response: Response) -> None:
     :param response: response
     :return: none
     """
+
+    con.writer.write(b'HTTP/1.1 ' + response.status.__bytes__() + b'\r\n')
+    for key, value in response.headers.items():
+        con.writer.write(f'{key}: {value}\r\n'.encode("utf-8"))
+    con.writer.write(b'\r\n')
+
+    if isinstance(response.data, bytes):
+        con.writer.write(response.data)
+    elif isinstance(response.data, Iterable):
+        for data in response.data:
+            con.writer.write(data)
+            if con.writer.transport.get_write_buffer_size() >= WRITE_BUFFER_SIZE:
+                await con.writer.drain()
+    elif isinstance(response.data, BytesIO):
+        try:
+            while data := response.data.read(WRITE_BUFFER_SIZE):
+                con.writer.write(data)
+                if con.writer.transport.get_write_buffer_size() >= WRITE_BUFFER_SIZE:
+                    await con.writer.drain()
+        except Exception as e:
+            response.data.close()
+            raise e
