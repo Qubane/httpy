f1d61718f305b2110e5af274ee23ed30be0154b5
UltraQbik <no1skill@yandex.ru>
2025-04-12 19:08:30 +0300

    add send_response function

--- source/network.py
+++ source/network.py
@@ -3,8 +3,7 @@ Networking stuff
 """
 
 
-import asyncio
-from source.classes import Connection, Request
+from source.classes import *
 
 
 async def fetch_request(con: Connection) -> Request:
@@ -12,4 +11,13 @@ async def fetch_request(con: Connection) -> Request:
     Fetches request from a connection
     :param con: client connection
     :return: client request
+    """
+
+
+async def send_response(con: Connection, response: Response) -> None:
+    """
+    Sends a response to a connection
+    :param con: connection
+    :param response: response
+    :return: none
     """
