094c621c32e73aaa3a51e69129e0b4c6f2258d92
UltraQbik <no1skill@yandex.ru>
2025-04-13 11:26:08 +0300

    add HTTPRequestError class

--- source/exceptions.py
+++ source/exceptions.py
@@ -7,3 +7,9 @@ class HTTPyException(Exception):
     """
     Base class for HTTPy server exceptions
     """
+
+
+class HTTPRequestError(HTTPyException):
+    """
+    Any type of HTTP request related error
+    """
