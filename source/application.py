1270a362a71543b5310e562168188120b25e86f7
UltraQbik <no1skill@yandex.ru>
2025-04-13 15:27:36 +0300

    make things work

--- source/application.py
+++ source/application.py
@@ -3,7 +3,9 @@ Main application file
 """
 
 
+import ssl
 import asyncio
+from source.server import *
 
 
 class App:
@@ -11,42 +13,47 @@ class App:
     Main application class
     """
 
-    def __init__(self, address: tuple[str, int], ssl: tuple[str, str] | None = None):
-        self.running_task: asyncio.Task | None = None
-
-        self.address: tuple[str, int] | None = address
-        self.ssl: tuple[str, str] | None = ssl
-
+    def __init__(self, address: tuple[str, int], ssl_keys: tuple[str, str] | None = None):
         self.server: asyncio.Server | None = None
+
+        # assign address and define ssl context
+        self.address: tuple[str, int] | None = address
+        self.ctx: ssl.SSLContext | None = None
+
+        # make context if needed
+        if ssl_keys and ssl_keys[0] and ssl_keys[1]:
+            self.ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER, check_hostname=False)
+            self.ctx.load_cert_chain(certfile=ssl_keys[0], keyfile=ssl_keys[1])
 
     def run(self) -> None:
         """
         Runs the application
         """
 
-        asyncio.run(self.run_coro())
-
-    async def run_coro(self):
-        """
-        Runs the application. Is a coroutine
-        """
-
-        # create new task
-        try:
-            self.running_task = asyncio.create_task(self._run_server())
-        except asyncio.CancelledError:
-            pass
+        asyncio.run(self._run_server())
 
     def quit(self) -> None:
         """
         Quits the application
         """
 
-        self.running_task.cancel()
+        self.server.close()
+        for sock in self.server.sockets:
+            sock.close()
 
     async def _run_server(self):
         """
         Run server coroutine
         """
 
+        self.server = await asyncio.start_server(
+            client_connected_cb=accept_client,
+            host=self.address[0],
+            port=self.address[1],
+            ssl=self.ctx)
 
+        async with self.server:
+            try:
+                await self.server.serve_forever()
+            except asyncio.exceptions.CancelledError:
+                pass
