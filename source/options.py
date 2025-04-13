3d304b85b68d872ae418f3b9d7b3f601b3c1aa2a
UltraQbik <no1skill@yandex.ru>
2025-04-13 18:27:37 +0300

    add directories

--- source/options.py
+++ source/options.py
@@ -3,4 +3,21 @@ Some configurations for the HTTPy server
 """
 
 
+import os
+
+
+# internal configurations
 WRITE_BUFFER_SIZE: int = 2 ** 10 * 16
+
+# directories
+TEMP_DIRECTORY: str = "temp"
+LOGS_DIRECTORY: str = "logs"
+ASSETS_DIRECTORY: str = "assets"
+
+# ensure directories exist
+if not os.path.isdir(TEMP_DIRECTORY):
+    os.makedirs(TEMP_DIRECTORY)
+if not os.path.isdir(LOGS_DIRECTORY):
+    os.makedirs(LOGS_DIRECTORY)
+if not os.path.isdir(ASSETS_DIRECTORY):
+    os.makedirs(ASSETS_DIRECTORY)
