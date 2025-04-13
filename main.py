fae729ddc121f44acad40e8ee4923bf76ad5aa8d
UltraQbik <no1skill@yandex.ru>
2025-04-13 18:33:12 +0300

    add initialization code

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ Main entrance file
 """
 
 
+from source.options import initialize
 from source.application import App
 
 
 def main():
+    initialize()
+
     app = App(
         address=("0.0.0.0", 8080),
         ssl_keys=None)
