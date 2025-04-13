635c42acbf153ebf483aa7573a67853ff742abb3
UltraQbik <no1skill@yandex.ru>
2025-04-13 18:32:20 +0300

    carry code to function

--- source/options.py
+++ source/options.py
@@ -16,24 +16,30 @@ TEMP_DIRECTORY: str = "temp"
 LOGS_DIRECTORY: str = "logs"
 ASSETS_DIRECTORY: str = "assets"
 
-# ensure directories exist
-if not os.path.isdir(TEMP_DIRECTORY):
-    os.makedirs(TEMP_DIRECTORY)
-if not os.path.isdir(LOGS_DIRECTORY):
-    os.makedirs(LOGS_DIRECTORY)
-if not os.path.isdir(ASSETS_DIRECTORY):
-    os.makedirs(ASSETS_DIRECTORY)
 
-# logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    datefmt="%Y-%m-%d %H:%M:%S",
-    style="{",
-    format="[{asctime}] [{levelname:<8}] {name}: {message}",
-    handlers=[
-        logging.handlers.RotatingFileHandler(
-            filename=f"{LOGS_DIRECTORY}/server.log",
-            encoding="utf-8",
-            maxBytes=2**20 * 2,  # 2 MiB
-            backupCount=5),
-        logging.StreamHandler()])
+def initialize():
+    """
+    Initializes some of the options
+    """
+
+    # ensure directories exist
+    if not os.path.isdir(TEMP_DIRECTORY):
+        os.makedirs(TEMP_DIRECTORY)
+    if not os.path.isdir(LOGS_DIRECTORY):
+        os.makedirs(LOGS_DIRECTORY)
+    if not os.path.isdir(ASSETS_DIRECTORY):
+        os.makedirs(ASSETS_DIRECTORY)
+
+    # logging
+    logging.basicConfig(
+        level=logging.DEBUG,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{",
+        format="[{asctime}] [{levelname:<8}] {name}: {message}",
+        handlers=[
+            logging.handlers.RotatingFileHandler(
+                filename=f"{LOGS_DIRECTORY}/server.log",
+                encoding="utf-8",
+                maxBytes=2**20 * 2,  # 2 MiB
+                backupCount=5),
+            logging.StreamHandler()])
