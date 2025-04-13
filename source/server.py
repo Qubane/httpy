9fc7f57e0bc36d97758c5218c49ebc6d86636e5d
UltraQbik <no1skill@yandex.ru>
2025-04-13 13:51:17 +0300

    add accept_client function

--- source/server.py
+++ source/server.py
@@ -3,8 +3,11 @@ HTTPy server code
 """
 
 
+import asyncio
 from source.classes import *
 from source.network import *
+from source.exceptions import *
+from source.status_codes import *
 
 
 class ClientHandle:
@@ -15,9 +18,10 @@ class ClientHandle:
     def __init__(self, con: Connection):
         self.con: Connection = con
 
-    async def handle(self) -> None:
+    async def handle(self) -> Response:
         """
         Handle the client
+        :return: response to client
         """
 
         request = fetch_request(self.con)
@@ -28,3 +32,40 @@ class ClientHandle:
         """
 
         self.con.writer.close()
+
+
+async def accept_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+    """
+    Accepts the client connection
+    :param reader: client reader
+    :param writer: client writer
+    """
+
+    # make connection and client handle
+    connection: Connection = Connection(reader, writer)
+    client_handle: ClientHandle = ClientHandle(connection)
+
+    # define response
+    response: Response | None = None
+
+    # try generating a response
+    try:
+        response = await client_handle.handle()
+
+    # error handling for external (non-server related) problems
+    except ExternalServerError as e:
+        response = Response(status=e.status, data=e.status.message.encode("utf-8"))
+
+    # error handling for internal problems
+    except (Exception, InternalServerError) as e:
+        # TODO: add logging
+        response = Response(status=STATUS_CODE_INTERNAL_SERVER_ERROR)
+
+    # try sending response
+    try:
+        await send_response(connection, response)
+    except Exception:
+        ...
+
+    # close client handle
+    client_handle.close()
