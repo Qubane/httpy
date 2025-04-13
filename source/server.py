c2f8ae69231c71fe7155ff8848bc39bfd2fb5340
UltraQbik <no1skill@yandex.ru>
2025-04-13 13:34:40 +0300

    add ClientHandle class

--- source/server.py
+++ source/server.py
@@ -1,3 +1,30 @@
 """
 HTTPy server code
 """
+
+
+from source.classes import *
+from source.network import *
+
+
+class ClientHandle:
+    """
+    Client handling class
+    """
+
+    def __init__(self, con: Connection):
+        self.con: Connection = con
+
+    async def handle(self) -> None:
+        """
+        Handle the client
+        """
+
+        request = fetch_request(self.con)
+
+    def close(self) -> None:
+        """
+        Closes the handle
+        """
+
+        self.con.writer.close()
