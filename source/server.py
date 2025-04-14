c020c429b39728688e74d39a834eb4dc7c6418f1
UltraQbik <no1skill@yandex.ru>
2025-04-14 21:00:23 +0300

    define add_path method

--- source/server.py
+++ source/server.py
@@ -4,6 +4,7 @@ HTTPy server code
 
 
 from source.options import *
+from source.classes import *
 
 
 class Server:
@@ -12,4 +13,11 @@ class Server:
     """
 
     def __init__(self):
-        ...
+        self._path_tree: dict[str, dict | object] = dict()
+
+    def add_path(self, path: str, page: Page) -> None:
+        """
+        Adds path to path tree
+        :param path: path to add
+        :param page: page class reference
+        """
