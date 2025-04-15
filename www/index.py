bbf65599d43231afa3b8f126d40bbbd851551c7e
UltraQbik <no1skill@yandex.ru>
2025-04-15 22:59:39 +0300

    throw some code for testing

--- www/index.py
+++ www/index.py
@@ -5,3 +5,39 @@ adds paths:
 - '/'
 - '/index.html'
 """
+
+
+from source.classes import *
+from source.status_codes import *
+
+
+TEST_PAGE = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Qubane's page </title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+    <body>
+        <header> Hello World! </header>
+    </body>
+</html>
+"""
+
+
+class IndexPage(Page):
+    """
+    index.html
+    """
+
+    def __init__(self, server: Server):
+        server.add_path("/", self)
+        server.add_path("/index.html", self)
+
+    async def on_request(self, request: Request) -> Response:
+        page = TEST_PAGE.encode("utf-8")
+
+        return Response(
+            status=STATUS_CODE_OK,
+            data=page)
