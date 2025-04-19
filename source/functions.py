880502f323ed3d90c39ea1868aa49e78e9860e71
UltraQbik <no1skill@yandex.ru>
2025-04-19 16:34:41 +0300

    add read_refactor_template

--- source/functions.py
+++ source/functions.py
@@ -1,3 +1,13 @@
 """
 Some generic functions
 """
+
+
+def read_refactor_template(path: str, mode: str, **kwargs) -> bytes | str:
+    """
+    Reads and refactors a template file
+    :param path: path to file
+    :param mode: mode (rb or r)
+    :param kwargs: arguments to be refactored
+    :return: bytes or str, depending on mode
+    """
