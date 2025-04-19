451ad392a5115c0de6138e1b5dabbab65aac60db
UltraQbik <no1skill@yandex.ru>
2025-04-19 22:37:29 +0300

    add more stuff I swear this is not feature creep :<

--- source/functions.py
+++ source/functions.py
@@ -3,6 +3,8 @@ Some generic functions
 """
 
 
+import gzip
+import brotli
 from typing import TextIO
 from source.classes import *
 
@@ -61,27 +63,42 @@ def generate_lazy_response(
         text: str | bytes,
         content_type: str,
         status: StatusCode,
+        additional_headers: dict | None = None,
         content_encoding: str | None = None) -> Response:
     """
     Generates a lazy response with automatically assigned headers
     :param text: text
     :param content_type: content type
     :param status: response status
+    :param additional_headers: additional headers
     :param content_encoding: content encoding (compression)
     :return: Response
     """
 
+    # check type
     if isinstance(text, str):
         encoded_text = text.encode("utf-8")
-    else:
+    elif isinstance(text, bytes):
         encoded_text = text
+    else:
+        raise ValueError
 
+    # compress if needed
+    if content_encoding == "gzip":
+        encoded_text = gzip.compress(encoded_text)
+    elif content_encoding == "br":
+        encoded_text = brotli.compress(encoded_text)
+
+    # add headers
     headers = {
         "Content-Length": len(encoded_text),
         "Content-Type": content_type,
         "Server": "HTTPy"
     }
+    if additional_headers:
+        headers.update(additional_headers)
 
+    # return response
     return Response(
         status=status,
         headers=headers,
