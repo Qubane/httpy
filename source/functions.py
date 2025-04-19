756b971cb52c3217a1510425b2fd564544be55de
UltraQbik <no1skill@yandex.ru>
2025-04-19 16:46:30 +0300

    make read_refactor_template

--- source/functions.py
+++ source/functions.py
@@ -3,11 +3,28 @@ Some generic functions
 """
 
 
-def read_refactor_template(path: str, mode: str, **kwargs) -> bytes | str:
+def read_refactor_template(path: str, **kwargs) -> bytes | str:
     """
     Reads and refactors a template file
     :param path: path to file
-    :param mode: mode (rb or r)
     :param kwargs: arguments to be refactored
     :return: bytes or str, depending on mode
     """
+
+    key_args = {f"{{{x}}}" for x in kwargs.keys()}
+
+    # output
+    page = ""
+
+    # read file
+    with open(path, "r", encoding="utf-8") as f:
+        # go through each line
+        for line in f.readlines():
+            # check if line argument is in key_args
+            if (arg := line.strip()) in key_args:
+                page += kwargs[arg[1:-1]]
+            else:
+                page += line
+
+    # return page
+    return page
