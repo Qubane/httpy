4ef19c785073e071ccb7fc46fc8ed10c0a5c5dee
UltraQbik <no1skill@yandex.ru>
2025-04-20 15:05:52 +0300

    work towards compression

--- source/functions.py
+++ source/functions.py
@@ -64,17 +64,19 @@ def generate_lazy_response(
         data: str | bytes,
         content_type: str,
         status: StatusCode,
-        additional_headers: dict | None = None,
-        content_encoding: str | None = None) -> Response:
+        additional_headers: dict[str, Header] | None = None) -> Response:
     """
     Generates a lazy response with automatically assigned headers
     :param data: text
     :param content_type: content type
     :param status: response status
     :param additional_headers: additional headers
-    :param content_encoding: content encoding (compression)
     :return: Response
     """
+
+    # check None
+    if additional_headers is None:
+        additional_headers = {}
 
     # check type
     if isinstance(data, str):
@@ -84,20 +86,25 @@ def generate_lazy_response(
     else:
         raise ValueError
 
-    # compress if needed
-    if content_encoding == "gzip":
-        encoded_text = gzip.compress(encoded_text)
-    elif content_encoding == "br":
-        encoded_text = brotli.compress(encoded_text)
-
     # add headers
     headers = {
-        "Content-Length": len(encoded_text),
-        "Content-Type": content_type,
-        "Server": "HTTPy"
-    }
-    if additional_headers:
-        headers.update(additional_headers)
+        "content-type": Header(content_type),
+        "server": Header("HTTPy")}
+
+    # add additional headers
+    headers.update(additional_headers)
+
+    # compress if needed
+    if "content-encoding" in additional_headers:
+        if "br" in additional_headers["content-encoding"].data:
+            encoded_text = gzip.compress(encoded_text)
+            headers["content-encoding"] = Header("br")
+        elif "gzip" in additional_headers["content-encoding"].data:
+            encoded_text = brotli.compress(encoded_text)
+            headers["content-encoding"] = Header("gzip")
+
+    # add length header
+    headers["content-length"] = Header(len(encoded_text))
 
     # return response
     return Response(
