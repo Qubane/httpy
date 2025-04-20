c4edf10906742513d729666ccef496ced04da706
UltraQbik <no1skill@yandex.ru>
2025-04-20 10:51:35 +0300

    add about

---
+++ www/about/about.py
@@ -0,0 +1,38 @@
+"""
+HTTPy index web page.
+
+adds paths:
+- '/about'
+"""
+
+
+from source.server import *
+from source.options import *
+from source.classes import *
+from source.functions import *
+from source.status_codes import *
+
+
+class AboutPage(Page):
+    """
+    about
+    """
+
+    def __init__(self, server: Server):
+        server.add_path("/about", self)
+
+    async def on_request(self, request: Request) -> Response:
+        with open(f"{WWW_DIRECTORY}/about/about_body.html", "r", encoding="utf-8") as f:
+            body = f.read()
+
+        with open(f"{ASSETS_DIRECTORY}/template.html", "r", encoding="utf-8") as f:
+            page = read_refactor_template(f, head="", body=body)
+
+        return generate_lazy_response(
+            text=page,
+            content_type="text/html",
+            status=STATUS_CODE_OK)
+
+
+def setup(server: Server) -> None:
+    server.add_page(AboutPage(server))
