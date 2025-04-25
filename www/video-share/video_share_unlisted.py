3661bc9cf866eb38351c0fc5ec6656c3f00729cc
UltraQbik <no1skill@yandex.ru>
2025-04-25 22:41:31 +0300

    make video_share_unlisted.py functional

--- www/video-share/video_share_unlisted.py
+++ www/video-share/video_share_unlisted.py
@@ -22,6 +22,7 @@ class VideoShareUnlistedPage(Page):
     """
 
     dir_path: str = f"{WWW_DIRECTORY}/video-share/unlisted_videos"
+    _update_videos_routine_interval: float = 5 * 60  # 5 minutes (* 60 seconds)
 
     def __init__(self, server: Server):
         server.add_path("/videos/unlisted/*", self)
@@ -33,16 +34,36 @@ class VideoShareUnlistedPage(Page):
 
     async def _update_videos_routine(self):
         """
-        Checks for new videos every 5 minutes
+        Checks for new videos
         """
 
         while True:
-            for file in glob.glob(self.dir_path + "/**/*", recursive=True):
-                print(file)
-            await asyncio.sleep(5 * 60)
+            for filepath in glob.glob(self.dir_path + "/**/*", recursive=True):
+                filepath = filepath.replace("\\", "/")
+                video_name = os.path.basename(os.path.splitext(filepath)[0])
+                self._video_list[video_name] = filepath
+            await asyncio.sleep(self._update_videos_routine_interval)
 
     async def on_request(self, request: Request) -> Response:
-        return Response(data=b'yes')
+        if (video_id := request.query_args.get("v")) is not None and video_id in self._video_list:
+            video = open(self._video_list[video_id], "rb")
+            video_type = os.path.splitext(self._video_list[video_id])[1]
+
+            # generate basic headers
+            video.seek(0, os.SEEK_END)
+            headers = {
+                "content-length": Header(video.tell()),
+                "content-type": Header(f"video/{video_type}")}
+            video.seek(0)
+
+            # return response
+            return Response(
+                status=STATUS_CODE_OK,
+                data=video,
+                headers=headers)
+        return Response(
+            status=STATUS_CODE_NOT_FOUND,
+            data=b'video not found')
 
 
 def setup(server: Server) -> None:
