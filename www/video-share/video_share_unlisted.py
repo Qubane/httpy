63bc3e9d8a14ea8771c39343159a46b3cad8f844
UltraQbik <no1skill@yandex.ru>
2025-04-25 22:29:02 +0300

    define _update_videos_routine

--- www/video-share/video_share_unlisted.py
+++ www/video-share/video_share_unlisted.py
@@ -6,6 +6,9 @@ adds paths:
 """
 
 
+import os
+import glob
+import asyncio
 from source.server import *
 from source.options import *
 from source.classes import *
@@ -15,15 +18,36 @@ from source.status_codes import *
 
 class VideoShareUnlistedPage(Page):
     """
-    example.file
+    video-share unlisted
     """
+
+    dir_path: str = f"{WWW_DIRECTORY}/video-share/unlisted_videos"
 
     def __init__(self, server: Server):
         server.add_path("/videos/unlisted/*", self)
 
+        # "uuid": "path/to/file"
+        self._video_list: dict[str, str] = {}
+
+        asyncio.create_task(self._update_videos_routine())
+
+    async def _update_videos_routine(self):
+        """
+        Checks for new videos every 5 minutes
+        """
+
+        while True:
+            for file in glob.glob(self.dir_path + "/**/*", recursive=True):
+                print(file)
+            await asyncio.sleep(5 * 60)
+
     async def on_request(self, request: Request) -> Response:
-        raise NotImplementedError
+        return Response(data=b'yes')
 
 
 def setup(server: Server) -> None:
+    # ensure path exists
+    if not os.path.isdir(VideoShareUnlistedPage.dir_path):
+        os.makedirs(VideoShareUnlistedPage.dir_path)
+
     server.add_page(VideoShareUnlistedPage(server))
